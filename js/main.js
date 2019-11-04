f6f38289ede5812f7da06f6fad45c81221932e48
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-04 21:24:22 +0200

    added animation for card2 in feature section

--- js/main.js
+++ js/main.js
@@ -6,8 +6,31 @@ if ($(window).width() >= 1024) {
     var ourScene = new ScrollMagic.Scene({
         triggerElement: '#card1',
         duration: 1000, // the scene should last for a scroll distance of 100px
+        triggerHook: 0.8
         // offset: 50 // start this scene after scrolling for 50px
-    }).setClassToggle('#card1', 'fade-in').addTo(controller); // assign the scene to the controller
+    })
+        .setClassToggle('#card1', 'fade-in')
+        .addIndicators({
+            name: 'card scene',
+            colorTrigger: 'white',
+            colorStart: 'grey'
+        })
+        .addTo(controller); // assign the scene to the controller
+
+    // create a scene
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement: '#card2',
+        duration: 1000, // the scene should last for a scroll distance of 100px
+        triggerHook: 0.8
+        // offset: 50 // start this scene after scrolling for 50px
+    })
+        .setClassToggle('#card2', 'fade-in')
+        .addIndicators({
+            name: 'card scene',
+            colorTrigger: 'white',
+            colorStart: 'grey'
+        })
+        .addTo(controller); // assign the scene to the controller
 
 }
 
