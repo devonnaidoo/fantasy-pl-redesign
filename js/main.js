bf3c9aa6a0854a2f9e4de27dd7efac9de49e0a23
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-04 21:57:37 +0200

    added animation for stats section

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ if ($(window).width() >= 1024) {
     //Init ScrollMagic
     var controller = new ScrollMagic.Controller();
 
-    // create a scene
-    var ourScene = new ScrollMagic.Scene({
+    // Features Scenes
+    // Card 1
+    var cardScene1 = new ScrollMagic.Scene({
         triggerElement: '#card1',
-        duration: '150%', // the scene should last for a scroll distance of 100px
+        duration: '145%', // the scene should last for a scroll distance of 100px
         triggerHook: 0.9
-        // offset: 50 // start this scene after scrolling for 50px
     })
         .setClassToggle('#card1', 'fade-in')
         .addIndicators({
@@ -17,12 +17,11 @@ if ($(window).width() >= 1024) {
         })
         .addTo(controller); // assign the scene to the controller
 
-    // create a scene
-    var ourScene = new ScrollMagic.Scene({
+    // Card 2
+    var cardScene2 = new ScrollMagic.Scene({
         triggerElement: '#card2',
-        duration: '150%', // the scene should last for a scroll distance
+        duration: '145%', // the scene should last for a scroll distance
         triggerHook: 0.9
-        // offset: 50 // start this scene after scrolling for 50px
     })
         .setClassToggle('#card2', 'fade-in')
         .addIndicators({
@@ -31,6 +30,50 @@ if ($(window).width() >= 1024) {
             colorStart: 'grey'
         })
         .addTo(controller); // assign the scene to the controller
+    // Card Scene End
 
+    // Statistics Scene
+    // Card 1
+    var cardStats1 = new ScrollMagic.Scene({
+        triggerElement: '#card-stats-1',
+        duration: '145%', // the scene should last for a scroll distance of 100px
+        triggerHook: 0.9,
+        offset: 100
+    })
+        .setClassToggle('#card-stats-1', 'slide-in')
+        .addIndicators({
+            name: 'card scene',
+            colorTrigger: 'white',
+            colorStart: 'grey'
+        })
+        .addTo(controller); // assign the scene to the controller
+    // Card 2
+    var cardStats1 = new ScrollMagic.Scene({
+        triggerElement: '#card-stats-2',
+        duration: '145%', // the scene should last for a scroll distance of 100px
+        triggerHook: 0.9,
+        offset: 100
+    })
+        .setClassToggle('#card-stats-2', 'slide-in')
+        .addIndicators({
+            name: 'card scene',
+            colorTrigger: 'white',
+            colorStart: 'grey'
+        })
+        .addTo(controller); // assign the scene to the controller
+    // Card 3
+    var cardStats1 = new ScrollMagic.Scene({
+        triggerElement: '#card-stats-3',
+        duration: '145%', // the scene should last for a scroll distance of 100px
+        triggerHook: 0.9,
+        offset: 100
+    })
+        .setClassToggle('#card-stats-3', 'slide-in')
+        .addIndicators({
+            name: 'card scene',
+            colorTrigger: 'white',
+            colorStart: 'grey'
+        })
+        .addTo(controller); // assign the scene to the controller
 }
 
