89d4d231eb514c76987d84c6ecaa36f581aa3832
devonnaidoo <devonnaidoomultimedia@gmail.com>
2019-11-04 21:07:37 +0200

    js for animation works on desktop only

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-//Init ScrollMagic
-var controller = new ScrollMagic.Controller();
+if ($(window).width() >= 1024) {
+    //Init ScrollMagic
+    var controller = new ScrollMagic.Controller();
 
-// create a scene
-var ourScene = new ScrollMagic.Scene({
-    triggerElement: '#card1',
-    duration: 1000, // the scene should last for a scroll distance of 100px
-    // offset: 50 // start this scene after scrolling for 50px
-}).setClassToggle('#card1', 'fade-in').addTo(controller); // assign the scene to the controller
+    // create a scene
+    var ourScene = new ScrollMagic.Scene({
+        triggerElement: '#card1',
+        duration: 1000, // the scene should last for a scroll distance of 100px
+        // offset: 50 // start this scene after scrolling for 50px
+    }).setClassToggle('#card1', 'fade-in').addTo(controller); // assign the scene to the controller
+
+}
+
